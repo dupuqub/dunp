abe98bbb7dc01b44cd43000fa12f9764c8807bf8
= <dupuqub@gmail.com>
2018-06-20 22:15:06 -0300

    html page + reroot + size + fixes

--- index.js
+++ index.js
@@ -6,15 +6,18 @@
 const log = item => console.log (item)
 const table = item => console.table (item)
 
+const newArray = length => Array.from ({length})
+const validArray = array => typeOf (array) === `array` && array.length > 0
+
+//......................................................................................................................
+
 const get = query => document.querySelector (query)
 const getAll = query => document.querySelectorAll (query)
 
 const styler = item => `${item [0]}: ${item [1]}`
 const attributer = item => ` ${item [0]}="${item [1]}"`
 
-const newArray = length => Array.from ({length})
-const validArray = array => typeOf (array) === `array` && array.length > 0
-
+const size = item => item.getBoundingClientRect ()
 const write = text => ({at: box => get (box).innerHTML = text})
 
 //......................................................................................................................
@@ -50,9 +53,9 @@ const typeOf = item =>
 
 //......................................................................................................................
 
-const htmlify = brick =>
+const html = brick =>
 {
-  if (typeOf (brick) === `array`) return !validArray (brick) ? `` : brick.map (htmlify).reduce (sum)
+  if (typeOf (brick) === `array`) return !validArray (brick) ? `` : brick.map (html).reduce (sum)
   if (typeOf (brick) !== `object`) return brick
 
   const type = brick.type || `div`
@@ -63,14 +66,58 @@ const htmlify = brick =>
   const inner =
 
       validArray (brick.inner)
-    ? brick.inner.map (htmlify).reduce (sum)
+    ? brick.inner.map (html).reduce (sum)
     : typeOf (brick.inner) === `object`
-    ? htmlify (brick.inner)
+    ? html (brick.inner)
     : brick.inner
     ? brick.inner
     : ``
 
   return `<${type + id + classes + others + style}>${inner}</${type}>`
+}
+
+//......................................................................................................................
+
+const ratio = (box, space) =>
+{
+  const scale = {w: space.w / box.w, h: space.h / box.h}
+  const vertical = scale.w < scale.h
+
+  const w = vertical ? space.w : scale.h * box.w
+  const h = !vertical ? space.h : scale.w * box.h
+  const u = (w + h) / 3000
+
+  // "UNIT = (WIDTH + HEIGHT) / 3000"
+  // this is because the focus here is a 1080p screen (1920px by 1080px)
+  // in such a screen the value of a unit would be 1 because 1920 + 1080 = 3000
+
+  return {w, h, u}
+}
+
+//......................................................................................................................
+
+const reroot = stage =>
+{
+  const {w, h, u} = stage
+
+  const rooter = sheet =>
+  {
+    if (sheet.href !== null
+    && sheet.href.indexOf (`dunp/index.css`) !== -1)
+    {
+      sheet.cssRules [0].style.cssText =
+
+          `--w: ${w}px;`
+        + `--h: ${h}px;`
+        + `--u: ${u}px;`
+
+      return true
+    }
+  }
+
+  Array
+  .from (document.styleSheets)
+  .some (rooter)
 }
 
 //......................................................................................................................
