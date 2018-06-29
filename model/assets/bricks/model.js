96b94d16c51be4dfe8bf4e38e15512e04ffc652e
= <dupuqub@gmail.com>
2018-06-29 13:27:50 -0300

    better commentary

--- model/assets/bricks/model.js
+++ model/assets/bricks/model.js
@@ -6,11 +6,11 @@
 //
 // If a brick is an object it must follow the structure in the example below (the object returned by the function).
 //
-// Every part of the object may not exist, however if the type does not exist, its value will be "`div`".
+// Every part of the brick object may not exist, however if the type does not exist, its value will be "`div`".
 //
-// If it is an array, every member may be anything a brick can be.
+// If the brick is an array, every member may be anything a brick can be.
 //
-// Functions will return, so watch out for those pesky undefineds.
+// Functions will return, so watch out for those pesky `undefined`.
 //
 // Styles defined here overpower any definition in any CSS file.
 //
