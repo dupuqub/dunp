cd5bff29f2ac384f7d662d2304017ed95c1b7da2
= <dupuqub@gmail.com>
2018-08-04 16:06:32 -0300

    "dunp.array.matrix" => "dunp.matrix.new" + improvements

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ dunp.array.matrix = (depths, initial) =>
     return dunp.array.new(length).map(() => builder(depths[index + 1], index + 1))
   }
 
-  return builder(depths[0], 0)
+  const matrix =
+  {
+    depth: depths.length,
+    body: builder(depths[0], 0),
+  }
+
+  return matrix
 }
 
 //......................................................................................................................
