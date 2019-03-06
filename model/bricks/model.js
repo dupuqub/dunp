8a734b9d7ffc149148330b4da22e8a42cb5ae614
Fillipe Ogg <dupuqub@gmail.com>
2019-03-06 13:17:32 -0300

    updated brick model

--- model/bricks/model.js
+++ model/bricks/model.js
@@ -3,7 +3,7 @@
 
 //......................................................................................................................
 
-// A brick may be a NaN, null, undefined, string, number, boolean, array, function or object.
+// A brick may be NaN, null, undefined, string, number, boolean, array, function or object.
 // If a brick is an object, it must follow the structure in the example below (the object returned by the function).
 // Every part of the brick object may not exist, however if the key "tag" does not exist, its value will be "`div`".
 // If the brick is an array, every index may be anything a brick can be.
@@ -14,22 +14,29 @@
 // Example.
 
 project.bricks.model = () =>
-({
-  id: `brickModel`,
-  tag: `canvas`,
-  classes: [`center`, `column`],
-  extras:
-  [
-    [`width`, `600`],
-    [`height`, `400`],
-    [`onclick`, `console.log(\`clicked\`)`],
-    [`onmouseover`, `console.log(\`hovered\`)`],
-  ],
-  styles:
-  [
-    [`border`, `calc(var(--u) * 3) solid #FFF`],
-    [`background`, `#AAA`],
-  ],
-  inner: null // Can be anything a brick can be.
-})
+{
+  const click = () => console.log(`clicked: ${event.target.id}`)
+  const hover = () => console.log(`hovered: ${event.target.id}`)
+  const brick =
+  {
+    id: `brickModel`,
+    tag: `canvas`,
+    classes: [`center`, `column`],
+    extras:
+    [
+      [`width`, `600`],
+      [`height`, `400`],
+      [`onclick`, dunp.trigger(click)],
+      [`onmouseover`, dunp.trigger(hover)],
+    ],
+    styles:
+    [
+      [`border`, `calc(var(--u) * 3) solid #FFF`],
+      [`background`, `#AAA`],
+    ],
+    inner: null // Can be anything a brick can be.
+  }
+
+  return brick
+}
 
